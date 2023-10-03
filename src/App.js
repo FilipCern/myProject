10428e9dc37bc4928d20ac9369179c8f4ad052ed
Filip <filip.cernohorsky@seznam.cz>
2023-10-03 18:01:56 +0200

    adding styles

--- src/App.js
+++ src/App.js
@@ -3,25 +3,52 @@ import './App.css';
 import Header from './components/Header';
 import ProfilePic from './components/ProfilePic';
 import Detail from './components/Detail';
+import styled from 'styled-components';
 
+const HeaderContainer = styled.div`
+background-color: #333;
+color: #fff;
+padding: 20px;
+text-align: center;
+`;
 
+const ProfilePicContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
 
-/*const user = {
-  name: 'Filípek Černohorský',
-  imageUrl: 'https://www.giantbomb.com/a/uploads/scale_small/1/17172/1148489-crag_hack.png',
-  hobbies: ['Hraní Heroesů', 'Hraní Baldurs Gatů', 'Hraní jiných her']
-};*/
+const ProfileImage = styled.img`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  margin: 20px;
+`;
+
+const DetailContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`;
+
 function App() {
-  
+
 
   return (
     <div>
+      <HeaderContainer>
       <Header title="User Profile Page" />
+      </HeaderContainer>
+      <ProfilePicContainer>
       <ProfilePic imageUrl={data.imageUrl} altText={data.name} />
+      </ProfilePicContainer>
+      <DetailContainer>
       <Detail name={data.name} hobbies={data.hobbies} />
+      </DetailContainer>
     </div>
   );
-  
+
 }
 
 export default App;
